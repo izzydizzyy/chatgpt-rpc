5d87601e5e16d3c2d9a7295f1eb0551c0fac9b98
Izzy <isaiahboyd84@gmail.com>
2026-09-08 17:20:12 -0400

    add v0.2 ui theme

---
+++ UiTheme.cs
@@ -0,0 +1,15 @@
+using System.Drawing;
+
+namespace ChatGPTRpc;
+
+internal static class UiTheme
+{
+    public static readonly Color Background = Color.FromArgb(15, 15, 15);
+    public static readonly Color Surface = Color.FromArgb(27, 27, 27);
+    public static readonly Color SurfaceHover = Color.FromArgb(35, 35, 35);
+    public static readonly Color Border = Color.FromArgb(51, 51, 51);
+    public static readonly Color Text = Color.FromArgb(242, 242, 242);
+    public static readonly Color Muted = Color.FromArgb(158, 158, 158);
+    public static readonly Color Accent = Color.FromArgb(118, 171, 152);
+    public static readonly Color Danger = Color.FromArgb(223, 100, 100);
+}
